8f401aa0ec9558b64aefc230e345dd569c4da2e8
Felix Aristo <felixaristo9@gmail.com>
2019-12-28 22:29:23 +0700

    Update soalinterview.py

--- soalinterview.py
+++ soalinterview.py
@@ -1,16 +1,17 @@
 # Author Felix Aristo
 # Python file for palindrome detection
 
-def reverseWords(s): 
-    return s[::-1]
-
 def palindromeDetection(s):
-    wordsReversed = reverseWords(s)
+    wordsReversed = s[::-1]
     
     if (s == wordsReversed):
         return True
     return False
     
-palindromeDetection("abcba")
+palindromeDetection("abcba") # Run the function
 
 # Return True
+
+palindromeDetection("Abcba") # Run the function
+
+# Return False
